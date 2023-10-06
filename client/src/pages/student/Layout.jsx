987f61b450029965a69756f85d569f6230cabaeb
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-10-06 23:22:50 +0530

    working on student

--- client/src/pages/student/Layout.jsx
+++ client/src/pages/student/Layout.jsx
@@ -1,5 +1,28 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 export const Layout = () => {
-  return <div>Layout</div>;
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [token, navigate]);
+
+  return (
+    <>
+      <Navbar />
+      <div className="pt-20">
+        <main className="lg:ml-24 p-4">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Layout;
