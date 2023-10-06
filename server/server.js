556d6c435396f5696e1e5ff6e8bb36cbaa5eac3b
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-10-06 20:35:38 +0530

    added multer

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import asyncErrors from "express-async-errors";
 import { ErrorMiddleWare } from "./middleware/error.js";
-import adminRoutes from './routes/adminRoutes.js'
-import coordinatorRoutes from './routes/coordinatorRoutes.js'
-import studentRoutes from './routes/studentRoutes.js'
+import adminRoutes from "./routes/adminRoutes.js";
+import coordinatorRoutes from "./routes/coordinatorRoutes.js";
+import studentRoutes from "./routes/studentRoutes.js";
+import multer from "multer";
 
 dotenv.config();
 const app = express();
@@ -14,12 +15,19 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(asyncErrors)
+app.use(asyncErrors);
 app.use(ErrorMiddleWare);
 
-app.use('/admin/',adminRoutes)
-app.use('/coordinator/',coordinatorRoutes)
-app.use('/student/',studentRoutes)
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 25 * 1024 * 1024,
+  },
+});
+
+app.use("/admin/", upload.single("bulkUpload"), adminRoutes);
+app.use("/coordinator/", coordinatorRoutes);
+app.use("/student/", upload.single("profileImage"), studentRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Team Tangled Devs " });
