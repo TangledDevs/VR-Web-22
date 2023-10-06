09f06e1718a452995777e96c85714e269862dcf2
gnanachandra <vvit.20bq1a05p2@gmail.com>
2023-10-06 21:08:01 +0530

    admin routes file changed

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,16 +1,41 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/verifyJWT.js";
-import { addAdmin, addCoordinator, addPlacement, addStudent, deleteCoordinator, getAllCoordinators, getAllPlacementResults, getAllStudents, login, updateCoordinatorDetails, updateStudentDetails } from "../controllers/adminControllers.js";
+import {
+  addAdmin,
+  addCoordinator,
+  addPlacement,
+  addStudent,
+  deleteCoordinator,
+  getAllCoordinators,
+  getAllPlacementResults,
+  getAllStudents,
+  login,
+  updateCoordinatorDetails,
+  updateStudentDetails,
+  uploadPlacementResults,
+  uploadStudents,
+} from "../controllers/adminControllers.js";
 const router = express.Router();
 
-router.route('login').post(login)
+router.route("login").post(login);
 
-router.use(isAuthenticated)
-router.use(authorizeRoles(["admin"]))
+router.use(isAuthenticated);
+router.use(authorizeRoles(["admin"]));
 
-router.route('students').get(getAllStudents).post(addStudent).patch(updateStudentDetails)
-router.route('coordintaors').get(getAllCoordinators).patch(updateCoordinatorDetails).delete(deleteCoordinator).post(addCoordinator)
-router.route('placements').get(getAllPlacementResults).post(addPlacement)
-router.route('new').post(addAdmin)
+router
+  .route("students")
+  .get(getAllStudents)
+  .post(addStudent)
+  .patch(updateStudentDetails);
+router.route("students/upload").post(uploadStudents);
+router
+  .route("coordinators")
+  .get(getAllCoordinators)
+  .patch(updateCoordinatorDetails)
+  .delete(deleteCoordinator)
+  .post(addCoordinator);
+router.route("placements").get(getAllPlacementResults).post(addPlacement);
+router.route("placements/uploadResults").post(uploadPlacementResults);
+router.route("new").post(addAdmin);
 
 export default router;
