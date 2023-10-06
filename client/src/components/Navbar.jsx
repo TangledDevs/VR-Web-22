fa615f73adc3521db541531e0b44ca8f49438db9
Varshith-p <varshithpuligadda@gmail.com>
2023-10-06 20:48:37 +0530

    navbar ui modified

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,11 +6,6 @@ export function Navbar() {
     <div className="w-full fixed bg-white border-b h-20 border-b-blue-gray-50 z-50">
       <div className="mx-auto flex items-center justify-between px-4 py-2 lg:py-[18px] sm:px-6 lg:px-8 h-[87px]">
         <div className="inline-flex items-center space-x-2 cursor-pointer">
-          {/* <img
-            src={Logo}
-            alt="Vaaradhi Logo"
-            className="h-8 w-8 lg:h-11 lg:w-11"
-          /> */}
           <span className="text-xl lg:text-2xl font-bold">Job Gateway</span>
         </div>
         <div className="flex justify-center items-center space-x-8">
--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -4,10 +4,10 @@ import {
   MenuList,
   MenuItem,
   IconButton,
-  Avatar,
   Typography,
 } from "@material-tailwind/react";
 import { IoNotificationsOutline } from "react-icons/io5";
+import { XCircleIcon, CheckCircleIcon } from "@heroicons/react/20/solid";
 
 function ClockIcon() {
   return (
@@ -38,15 +38,10 @@ export default function NotificationsMenu() {
       </MenuHandler>
       <MenuList className="flex flex-col gap-2">
         <MenuItem className="flex items-center gap-4 py-2 pr-8 pl-2">
-          <Avatar
-            variant="circular"
-            alt="tania andrew"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-          />
+          <CheckCircleIcon className="w-8 h-8 text-green-500" />
           <div className="flex flex-col gap-1">
             <Typography variant="small" color="gray" className="font-normal">
-              <span className="font-medium text-blue-gray-900">Tania</span> send
-              you a message
+              You&apos;re selected in Amazon
             </Typography>
             <Typography
               variant="small"
@@ -58,40 +53,16 @@ export default function NotificationsMenu() {
           </div>
         </MenuItem>
         <MenuItem className="flex items-center gap-4 py-2 pr-8 pl-2">
-          <Avatar
-            variant="circular"
-            alt="natali craig"
-            src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1061&q=80"
-          />
+          <XCircleIcon className="w-8 h-8 text-red-500" />
           <div className="flex flex-col gap-1">
             <Typography variant="small" color="gray" className="font-normal">
-              <span className="font-medium text-blue-gray-900">Natali</span>{" "}
-              reply to your email
+              Your offer letter is invalid
             </Typography>
             <Typography
               variant="small"
               className="flex items-center gap-1 text-xs text-gray-600"
             >
               <ClockIcon />a hour ago
-            </Typography>
-          </div>
-        </MenuItem>
-        <MenuItem className="flex items-center gap-4 py-2 pr-8 pl-2">
-          <Avatar
-            variant="circular"
-            alt="paypal"
-            src="https://dwglogo.com/wp-content/uploads/2016/08/PayPal_Logo_Icon.png"
-          />
-          <div className="flex flex-col gap-1">
-            <Typography variant="small" color="gray" className="font-normal">
-              <span className="font-medium text-blue-gray-900">PayPal</span>{" "}
-              you&apos;ve received a payment.
-            </Typography>
-            <Typography
-              variant="small"
-              className="flex items-center gap-1 text-xs text-gray-600"
-            >
-              <ClockIcon />5 hours ago
             </Typography>
           </div>
         </MenuItem>
